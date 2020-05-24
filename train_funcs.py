98bcf48ba874c4e2ee4478266d2b71d2782ee64b
KeremOzfo <52282189+KeremOzfo@users.noreply.github.com>
2020-05-25 01:47:04 +0300

    Add files via upload

--- train_funcs.py
+++ train_funcs.py
@@ -8,6 +8,7 @@ import math
 from parameters import *
 import time
 import numpy as np
+from tqdm import tqdm
 
 
 def evaluate_accuracy(model, testloader, device):
@@ -22,7 +23,6 @@ def evaluate_accuracy(model, testloader, device):
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    print(correct/total*100)
     return correct / total
 
 
@@ -53,57 +53,62 @@ def train(args, device):
     sf.initialize_zero(net_ps)
     for cl in range(num_client):
         errors.append(torch.zeros(modelsize).to(device))
-    testEpoch = 0
-    runs = math.ceil(N_s*args.num_epoch/(args.bs*num_client))
-    for run in range(runs):
-        epoch = math.floor(run * args.bs * num_client / N_s)
+    runs = math.ceil(N_s/(args.bs*num_client))
+
+    acc = evaluate_accuracy(net_ps, testloader, device)
+    accuracys.append(acc * 100)
+    for epoch in tqdm(range(args.num_epoch)):
         for cl in range(num_client):
             sf.adjust_learning_rate(optimizers[cl], epoch, args.lr)
-        currentLR = sf.get_LR(epoch,args.lr)
-        for cl in range(num_client):
+        currentLR = sf.get_LR(epoch, args.lr)
 
-            trainloader = DataLoader(dl.DatasetSplit(trainset, sample_inds[cl]), batch_size=args.bs,
-                                     shuffle=True)
-            for data in trainloader:
-                inputs, labels = data
-                inputs, labels = inputs.to(device), labels.to(device)
-                optimizers[cl].zero_grad()
-                predicts = net_users[cl](inputs)
-                loss = criterions[cl](predicts, labels)
-                loss.backward()
-                optimizers[cl].step()
-                break
-        ps_model_flat = sf.get_model_flattened(net_ps, device)
-        ps_model_dif = torch.zeros_like(ps_model_flat)
-        for cl in range(num_client):
-            model_flat = sf.get_model_flattened(net_users[cl], device)
-            difmodel = (model_flat.sub(ps_model_flat)).to(device)
-            difmodel.add_(errors[cl] * currentLR)
-            difmodel_clone = torch.clone(difmodel).to(device)
-            #print(difmodel)
-            
+
+        for run in range(runs):
+
+            for cl in range(num_client):
+
+                trainloader = DataLoader(dl.DatasetSplit(trainset, sample_inds[cl]), batch_size=args.bs,
+                                         shuffle=True)
+                for data in trainloader:
+                    inputs, labels = data
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    optimizers[cl].zero_grad()
+                    predicts = net_users[cl](inputs)
+                    loss = criterions[cl](predicts, labels)
+                    loss.backward()
+                    optimizers[cl].step()
+                    break
+            ps_model_flat = sf.get_model_flattened(net_ps, device)
+            ps_model_dif = torch.zeros_like(ps_model_flat)
+            for cl in range(num_client):
+                model_flat = sf.get_model_flattened(net_users[cl], device)
+                difmodel = (model_flat.sub(ps_model_flat)).to(device)
+                difmodel.add_(errors[cl] * currentLR)
+                difmodel_clone = torch.clone(difmodel).to(device)
+                #print(difmodel)
+
+                if args.layer_wise_spars:
+                    sf.sparse_timeC_alt(difmodel,args.sparsity_window,10,ps_model_mask,ind_pairs,device)
+                else:
+                    sf.sparse_timeC(difmodel, args.sparsity_window, 10, ps_model_mask, device)
+
+                errors[cl] = (difmodel_clone.sub(difmodel)) / currentLR
+                ps_model_dif.add_(difmodel/num_client)
+            ps_model_flat.add_(ps_model_dif)
+            topk = math.ceil(ps_model_dif.nelement() / args.sparsity_window)
+            ind = torch.topk(ps_model_dif.abs(), k=topk, dim=0)[1]
+
             if args.layer_wise_spars:
-                sf.sparse_timeC_alt(difmodel,args.sparsity_window,10,ps_model_mask,ind_pairs,device)
+                ps_model_mask = sf.sparse_special_mask(ps_model_flat, args.sparsity_window, 1000, ind_pairs, device)
             else:
-                sf.sparse_timeC(difmodel, args.sparsity_window, 10, ps_model_mask, device)
+                ps_model_mask *= 0
+                ps_model_mask[ind] = 1
 
-            errors[cl] = (difmodel_clone.sub(difmodel)) / currentLR
-            ps_model_dif.add_(difmodel/num_client)
-        ps_model_flat.add_(ps_model_dif)
-        topk = math.ceil(ps_model_dif.nelement() / args.sparsity_window)
-        val, ind = torch.topk(ps_model_dif.abs(), k=topk, dim=0)
+            sf.make_model_unflattened(net_ps, ps_model_flat, net_sizes, ind_pairs)
 
-        if args.layer_wise_spars:
-            ps_model_mask = sf.sparse_special_mask(ps_model_flat, args.sparsity_window, 1000, ind_pairs, device)
-        else:
-            ps_model_mask = torch.zeros_like(ps_model_flat)
-            ps_model_mask[ind] = 1
+            [sf.pull_model(net_users[cl], net_ps) for cl in range(num_client)]
 
-        sf.make_model_unflattened(net_ps, ps_model_flat, net_sizes, ind_pairs)
-
-        [sf.pull_model(net_users[cl], net_ps) for cl in range(num_client)]
-        if testEpoch == epoch:
-            acc = evaluate_accuracy(net_ps, testloader, device)
-            accuracys.append(acc*100)
-            testEpoch +=1
+        acc = evaluate_accuracy(net_ps, testloader, device)
+        accuracys.append(acc * 100)
+        print('accuracy: ',acc*100,)
     return accuracys
